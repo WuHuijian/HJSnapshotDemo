13e1552b4d0925a69fdc874f2e3a221c3a36d76e
WHJ <760503653@qq.com>
2018-01-10 18:06:59 +0800

    Initial Commit

---
+++ HJSnapshotDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJSnapshotDemo
+//
+//  Created by WHJ on 2018/1/10.
+//  Copyright © 2018年 WHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJSnapshotDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJSnapshotDemo
+//
+//  Created by WHJ on 2018/1/10.
+//  Copyright © 2018年 WHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
